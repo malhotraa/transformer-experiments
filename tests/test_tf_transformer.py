f08ef88e0384c2835e63ff01c2c5c7fc37b0acdd
Jovan Sardinha <jovan.sardinha@gmail.com>
2021-03-06 19:50:21 -0800

    added stanard code and tests for MultiHeadedAttention

--- tests/test_tf_transformer.py
+++ tests/test_tf_transformer.py
@@ -3,27 +3,20 @@ import unittest
 import tensorflow as tf
 import numpy.testing as npt
 
-from transformer.tf.model import attention
+from transformer.tf.model import attention, MultiHeadAttention
 
 class TestAttention(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
 
     def test_attention(self):
         temp_k = tf.constant([[10,0,0],
-                      [0,10,0],
-                      [0,0,10],
-                      [0,0,10]], dtype=tf.float32)  # (4, 3)
+                [0,10,0],
+                [0,0,10],
+                [0,0,10]], dtype=tf.float32)  # (4, 3)
 
         temp_v = tf.constant([[   1,0],
                             [  10,0],
                             [ 100,5],
                             [1000,6]], dtype=tf.float32)  # (4, 2)
-
 
         temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)  # (1, 3)
         out, attn = attention(temp_q, temp_k, temp_v)
@@ -40,4 +33,13 @@ class TestAttention(unittest.TestCase):
         npt.assert_almost_equal(attn.numpy(), [[0.5, 0.5, 0., 0.]], 0)
         npt.assert_almost_equal(out.numpy(), [[5.5, 0.]], 0)
 
+class TestMultiHeadedAttention(unittest.TestCase):
+
+    def test_multi_head_attention(self):
+
+        temp_mha = MultiHeadAttention(d_model=512, num_heads=8)
+        y = tf.random.uniform((1, 60, 512))  # (batch_size, encoder_sequence, d_model)
+        out, attn = temp_mha(y, k=y, q=y, mask=None)
+        self.assertEqual(out.shape, (1, 60, 512))
+        self.assertEqual(attn.shape, (1, 8, 60, 60))
 
